0c483ae57b1b66173d4962cf699e9de66b536951
prince9118 <pchaudharyskn@gmail.com>
2026-08-29 23:15:23 +0530

    healthApi and db

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { prisma } from "./lib/prisma.js";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.get("/api/health", (_req, res) => {
     message: "Server is running"
   });
 });
+
 
 export default app;
